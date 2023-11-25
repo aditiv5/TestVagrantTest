06dcdff9521c7f6e73ed0950d60564bef2949482
Aditi V <aditiv590@gmail.com>
2023-11-25 14:21:47 +0530

    Update shopping cart.py

--- shopping cart.py
+++ shopping cart.py
@@ -13,15 +13,15 @@ def gst(amount,gst):
 
 product = [["Leather wallet",1100,18,1],["Umbrella",900,12,4],["cigarette",200,28,3],["Honey",100,0,2]]
 length = len(product)
-final_price = []
+final = []
 max_gst = []
-final_amount = []
+final = []
 for i in range(length):
     #for j in range(i+1):
     result1 = discount(product[i][1],product[i][3])
     result2 = gst(result1,product[i][2])
     final_price1 = result1 + result2
-    final_amount.append(final_price1)
+    final.append(final_price1)
     max_gst.append(result2)
-print("Total amount to be paid to shop-keeper:",sum(final_amount))
+print("Total amount to be paid to shop-keeper:",sum(final))
 print("Maximim GST amount: ",max(max_gst),"for the product",product[1][0])
